38297d34e801693b12f656a4dc640e9ef8e8c2c3
Isaac Low <isaackarwailow@gmail.com>
2023-02-22 21:24:14 +1100

    Update rand_easy_pw.py

--- rand_easy_pw.py
+++ rand_easy_pw.py
@@ -3,35 +3,29 @@ import random
 import string
 
 def generate_random_word():
-    prefixes = ['re', 'pre', 'post', 'un', 'dis']
-    suffixes = ['able', 'er', 'ish', 'ment', 'ness']
-    word = ''.join(random.choice(string.ascii_lowercase) for i in range(random.randint(3, 8)))
 
-    if random.choice([True, False]):
-        word = random.choice(prefixes) + word
-    else:
-        word = word + random.choice(suffixes)
-    
+    word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+    response = requests.get(word_site)
+    WORDS = response.content.splitlines()
+
+    number = random.randint(0, 9999)
+    # print out b'zus'
+    q_word = str(WORDS[number])
+    word = q_word.split("'")[1]
+    # there are 10000 words in this url dictionary
+    # generate a random number between 0 and 9999
+
     word = word.capitalize()
-    return word
+
+    return word # Output: example string
+
 # password to be in the form XXXTTTTT004
 def generate_password():
     word1 = generate_random_word()
     word2 = generate_random_word()
 
-    number = str(random.randint(0, 999)).zfill(3)
+    number = '00' + str(random.randint(0, 9))
     password = word1 + word2 + number
     return password
 
-password = generate_password()
-print(password)
-
-rw = generate_random_word()
-print(rw)
-
-
-word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
-
-response = urllib2.urlopen(word_site)
-txt = response.read()
-WORDS = txt.splitlines()
+print(generate_password())
